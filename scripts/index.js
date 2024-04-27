7796a4974ecdf641a2f50f60151af65007d2c42a
bilal tazurkaev <keer158@mail.ru>
2024-04-27 15:33:36 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
+const cardContainer = document.querySelector('.places__list')
 
-// @todo: DOM узлы
+function creationCard(card,deleteCard) {
+    const cardTemplateClone = cardTemplate.querySelector('.places__item').cloneNode(true)
+    cardTemplateClone.querySelector('.card__image').src = card.link;
+    cardTemplateClone.querySelector('.card__title').textContent = card.name;
+    const deleteButton = cardTemplateClone.querySelector('.card__delete-button')
+    deleteButton.addEventListener('click', function() {
+        deleteCard(cardTemplateClone)
+    })
 
-// @todo: Функция создания карточки
+    return cardTemplateClone
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(card){
+card.remove()
+}
 
-// @todo: Вывести карточки на страницу
+for(let i = 0; i < initialCards.length; i++){
+    const addCard = creationCard(initialCards[i], deleteCard)
+    cardContainer.prepend(addCard)
+}
